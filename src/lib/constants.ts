6876e0e22d1f066ada9b10af6377c754bb759c0b
Abinay <abinayan.rajendran224@gmail.com>
2026-04-18 15:21:48 +0530

    updated constants phone number

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const COMPANY_NAME = "Smartflow";
 export const CONTACT_EMAIL = "hello@smartflow.lk";
-export const WHATSAPP_NUMBER = "947717752010";
+export const WHATSAPP_NUMBER = "94717752010";
 export const WHATSAPP_MESSAGE = "Hi, I'm interested in your services";
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
